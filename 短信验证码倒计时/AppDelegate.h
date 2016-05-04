a67303f592adf279efaf44f4054ce756a04e951d
DreamLCM <mac@mac.local>
2016-05-04 16:46:13 +0800

    Initial Commit

---
+++ 短信验证码倒计时/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  短信验证码倒计时
+//
+//  Created by MAC on 16/5/4.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 短信验证码倒计时/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  短信验证码倒计时
+//
+//  Created by MAC on 16/5/4.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
